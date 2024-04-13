f52f108d79c463fa4927bd7d2128a0f449f51f7a
MMy <m.myczka@gmail.com>
2024-04-13 06:40:43 +0200

    feat: select item by name and get all location paths

---
+++ db_relational/select_item_by_name.sql
@@ -0,0 +1,9 @@
+SELECT CONCAT( r.Name, '/',
+	COALESCE(c.Name + '/',''),
+	COALESCE(l.Name + '/',''),
+	i.Name) AS LocationPath
+FROM Item i
+	JOIN Location l ON i.LocationID = l.LocationID
+	JOIN Container c ON l.ContainerID = c.ContainerID
+	JOIN Room r ON c.RoomID = r.RoomID
+WHERE i.Name = 'Towels';
